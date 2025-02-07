4deb0325d4136d097d6af75c6407c901494d1bc0
ramos-vini <ramoscontrole@gmail.com>
2025-02-07 18:21:29 -0300

    create and destroy session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,4 +2,26 @@ class SessionsController < ApplicationController
   def new
   end
   
+  def create
+    user = User.find_by(username: params[:username_or_email])
+
+    user = User.find_by(email: params[:username_or_email]) if !user
+    
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      flash[:notice] = "You have successfully logged in."
+      redirect_to root_path
+    else
+      flash.now[:error] = "User credentials do not match."
+      render :new
+    end
+
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:notice] = "You have successfully logged out."
+    redirect_to root_path
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "chatroom#index"
 
-  get "login", to: "sessions#new" 
+  get "login", to: "sessions#new"
+  post "login", to: "sessions#create"
+  delete "logout", to: "sessions#destroy"
 end
